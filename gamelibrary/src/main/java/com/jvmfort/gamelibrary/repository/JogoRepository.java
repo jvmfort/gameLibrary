5eedb833ded418ea879bde66edff77766547dbf9
João V. M. Fortunato <jvmfortunato@gmail.com>
2026-05-17 15:57:02 -0300

    Delete gamelibrary directory

--- gamelibrary/src/main/java/com/jvmfort/gamelibrary/repository/JogoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jvmfort.gamelibrary.repository;
-
-import com.jvmfort.gamelibrary.model.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JogoRepository extends JpaRepository<Jogo, Long> {
-}
