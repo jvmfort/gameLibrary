e81c91897021b7a73415e33d45d619fac076418d
jvmfo <joao.monteiro@dcx.ufpb.br>
2026-09-14 10:45:22 -0300

    Recreation and little improvements to the back-end, and implemented the frontend

--- src/main/java/com/jvmfort/gamelibrary/exception/JogoNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.jvmfort.gamelibrary.exception;
-
-public class JogoNaoEncontradoException extends RuntimeException {
-    public JogoNaoEncontradoException(Long id) {
-        super("Jogo com ID " + id + " não encontrado");
-    }
-}
--- src/main/java/com/jvmfort/gamelibrary/model/StatusJogo.java
+++
@@ -1,9 +0,0 @@
-package com.jvmfort.gamelibrary.model;
-
-public enum StatusJogo {
-    NAO_INICIADO,
-    EM_ANDAMENTO,
-    CONCLUIDO,
-    ABANDONADO,
-    PLATINADO
-}
