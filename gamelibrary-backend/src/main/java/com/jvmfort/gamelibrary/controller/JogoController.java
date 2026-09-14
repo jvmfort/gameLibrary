ae13633d01bad8a70115bd8f0d2600cfef017818
jvmfo <joao.monteiro@dcx.ufpb.br>
2026-09-14 18:28:04 -0300

    Going Vercel

--- gamelibrary-backend/src/main/java/com/jvmfort/gamelibrary/controller/JogoController.java
+++ gamelibrary-backend/src/main/java/com/jvmfort/gamelibrary/controller/JogoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin(origins = {"http://localhost:5173", "https://seu-frontend.vercel.app"})
+@CrossOrigin(origins = {"http://localhost:5173", "https://game-library-teal.vercel.app/"})
 @RestController
 @RequestMapping("/jogos")
 
